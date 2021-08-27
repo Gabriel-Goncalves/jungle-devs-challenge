8b5412dc791b20a4c49abef512dac98dc779bfc1
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-08-27 18:10:12 -0300

    starting jungle devs challenge

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Badge from '../../images/headerLogo.svg';
+import './header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <section className="header-left-content">
+        <img src={Badge} alt="Badge" />
+        <ul>
+          <li>Create Your Nanny Share</li>
+          <li>Browse Shares</li>
+          <li>Our Story</li>
+        </ul>
+      </section>
+      <section className="header-right-content">
+        <ul>
+          <li>
+            <button type="button">Become a Nanny Share Host</button>
+          </li>
+          <li>
+            Sign In
+          </li>
+        </ul>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
